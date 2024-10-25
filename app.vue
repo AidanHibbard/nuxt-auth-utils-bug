670bf1487bfc556b588b29a53b45daa55e111bfb
Aidan Hibbard <aidanchibbard@gmail.com>
2024-10-24 21:32:55 -0700

    init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <p class="underline">Welcome!</p>
+    <AuthState>
+      <template #default="{ loggedIn, clear }">
+        <button v-if="loggedIn" @click="clear">Logout</button>
+        <NuxtLink v-else to="/auth/auth0">Login</NuxtLink>
+      </template>
+      <template #placeholder>
+        <button disabled>Loading...</button>
+      </template>
+    </AuthState>
+  </header>
+</template>
